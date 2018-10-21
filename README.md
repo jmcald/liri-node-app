c8a34203b9580ce12552b47908cceab51fa2c874
John Caldwell <jmcald0@gmail.com>
2018-10-20 23:15:43 -0500

    MVP completed

--- README.md
+++ README.md
@@ -1,5 +1,100 @@
 # liri-node-app
 A node app used for finding songs, bands, and movies
 
-needs node_modules
-needs dotemv file
+Use the command line in Node.js to find out information about songs, bands, and movies!
+
+First, to use this app, you must install "node_modules" as well as acquire your own "id" and "secret" keys from spotify to have full functionality. 
+
+"node_modules" must be installed after node.js. To install, simply type "npm install" into the command line. The package.JSON file should have all of the necessary dependencies listed, so all appropriate files will be downloaded.
+
+![npm_modules](./images/npminstall.png)
+
+For the Spotify keys, it is strongly recommended that you create your own ".emv" file to include your id and secret under. To easily insert them into the app, it is strongly recommended that the id be named "SPOTIFY_ID", and that the secret be named "SPOTIFY_SECRET". 
+
+![dotenv](./images/dotenv.png)
+
+From the command prompt, operating the app is fairly simple, but character sensitive. Each type of query will be detailed below.
+
+1. Finding concerts via "BandsInTown"
+
+    To find concerts by a certain artist, "node liri.js concert-this <artist-name-here>" must be typed into the command prompt
+
+![concert-this](/images/concert-this.png)
+
+    Once that is done, a short list of information will be displayed right in the terminal including:
+
+        The name of the venue they are playing at.
+
+        The location of the venue.
+
+        And, the date that they are playing.
+
+![concert-this-executed](/images/concert-this-executed.png)
+
+2. Finding songs via "Spotify"
+
+    To find concerts by a certain song, "node liri.js spotify-this-song <song-name-here>" must be typed into the command prompt
+
+![spotify-this-song](/images/spotify-this-song.png)
+
+    Once that is done, a short list of information will be displayed in the terminal including:
+
+        The artist(s) name.
+
+        The song's name.
+
+        A link to a preview of the song.
+
+        And, the name of the album that the song is on.
+
+![spotify-this-song-executed](/images/spotify-this-song-executed.png)
+
+    It is possibly to not give a song name and still get a result. 
+
+![spotify-this-song-no-argument](/images/spotify-this-song-no-argument.png)
+
+    When no song is given, the app defaults to "The Sign" by Ace of Base.
+
+![spotify-this-song-no-argument-executed](/images/spotify-this-song-no-argument-executed.png)
+
+3. Finding songs via "OMDB"
+
+    To find concerts by a certain movie, "node liri.js movie-this <movie-name-here>" must be typed into the command prompt
+
+![movie-this](/images/movie-this.png)
+
+    Once that is done, a short list of information will be displayed in the terminal including:
+
+        The title of the movie.
+
+        The year the movie came out.
+
+        The IMDB Rating for the movie.
+
+        The Rotten Tomatoes Rating of the movie.
+
+        The country where the movie was produced.
+
+        The language the movie was shot in.
+
+        The plot of the movie.
+
+        And the actors in the movie.
+
+![movie-this-executed](/images/movie-this-executed.png)
+
+    It is possibly to not give a movie name and still get a result. 
+
+![movie-this-no-argument](/images/movie-this-no-argument.png)
+
+    When no song is given, the app defaults to "Mr. Nobody".
+
+![movie-this-no-argument-executed](/images/movie-this-no-argument-executed.png)
+
+    It is also possible to feed the app information stored in a text file (.txt). To initiate the app to read and process the text file, "node liri.js do-what-it-says" must be typed in the command prompt.
+
+![do-what-it-says](/images/do-what-it-says.png)
+
+    It is necessary to put the desired text in a file called "random.text" in the same directory as the liri.js file for it to work, as is. Once the command is entered in the terminal, the text file gets read and the app performs the function described in the text file.
+
+![do-what-it-says-executed](/images/do-what-it-says-executed.png)
--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 const Spotify = require('node-spotify-api');
 
@@ -6,23 +6,289 @@ const request = require('request');
 
 const moment = require('moment'); 
 
-const keys = require("./keys")
+const keys = require("./keys");
 
-const spotify = new Spotify(keys.spotify);
+const fs = require("fs");
+
+const spotify = new Spotify(keys.spotifyKeys);
 
 process.argv.splice(0,2);
 
+let primaryArg = process.argv[0];
+
+let secondArg = process.argv.slice(1).join(" ");
+
+const divider = "\n------------------------------------------------------------\n\n"
+
+if (primaryArg === "do-what-it-says") {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+  
+    var dataArr = data.split(",");
+    console.log(dataArr)
+
+    let primaryArg = dataArr[0];
+
+    let secondArg = dataArr[1];
+
+    switch (primaryArg) {
+      // * `concert-this`
+        case "concert-this":
+        
+        function bandsInTown() {
+      
+          bandsURL = "https://rest.bandsintown.com/artists/" + secondArg + "/events?app_id=codingbootcamp";
+      
+          request(bandsURL, function(err, response, body) {
+              let jsonData = JSON.parse(body);
+      
+              let artistData = [
+                "Venue: " + jsonData[0].venue.name,
+                "Location: " + jsonData[0].venue.city + ", " + jsonData[0].venue.region,
+                "Date: " + moment(jsonData[0].datetime).format("MM/DD/YYYY")
+      
+              ].join("\n\n");
+      
+              fs.appendFile("log.txt", artistData + divider, function(err) {
+                if (err) throw err;
+                console.log("\n", artistData);
+              });
+          });
+      
+        };
+      
+        bandsInTown();
+        break;
+      // * `spotify-this-song`
+        case "spotify-this-song":
+      
+        
+        if (secondArg === "") {
+          secondArg = "The Sign"
+      
+          spotify.search({ type: "track", query: secondArg }, function(err, data) {
+      
+            let spotifyData = [
+              "Artist: " + data.tracks.items[7].album.artists[0].name,
+              "Name: " + data.tracks.items[7].name,
+              "Preview: " + data.tracks.items[7].preview_url,
+              "Album: " + data.tracks.items[7].album.name,
+            ].join("\n\n");
+        
+            fs.appendFile("log.txt", spotifyData + divider, function(err) {
+              if (err) throw err;
+              console.log("\n",spotifyData);
+            });
+          });
+        }
+        
+        else {
+          spotify.search({ type: "track", query: secondArg }, function(err, data) {
+      
+            let spotifyData = [
+              "Artist: " + data.tracks.items[0].album.artists[0].name,
+              "Name: " + data.tracks.items[0].name,
+              "Preview: " + data.tracks.items[0].preview_url,
+              "Album: " + data.tracks.items[0].album.name,
+            ].join("\n\n");
+      
+            fs.appendFile("log.txt", spotifyData + divider, function(err) {
+              if (err) throw err;
+              console.log("\n",spotifyData);
+            });
+          })};
+        break;
+      
+      // * `movie-this`
+        case "movie-this":
+      
+        function omdb() {
+      
+      
+          let movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + secondArg;
+      
+          if (secondArg === "") {
+            
+            let movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=Mr.Nobody"
+      
+          request(movieURL, function(err, response, body) {
+            let jsonData = JSON.parse(body);
+      
+            let movieData = [
+              "Title: " + jsonData.Title,
+              "Release Year: " + jsonData.Year,
+              "IMDB rating: " + jsonData.imdbRating,
+              "Rotten Tomatoes rating: " + jsonData.Ratings[1].Value,
+              "Country Where Movie was Produced: " + jsonData.Country,
+              "Language: " + jsonData.Language,
+              "Plot: " + jsonData.Plot,
+              "Actors: " + jsonData.Actors
+            ].join("\n\n");
+      
+            fs.appendFile("log.txt", movieData + divider, function(err) {
+              if (err) throw err;
+              console.log("\n",movieData);
+            });
+          })}
+      
+          else {
+      
+          request(movieURL, function(err, response, body) {
+            let jsonData = JSON.parse(body);
+      
+            let movieData = [
+              "Title: " + jsonData.Title,
+              "Release Year: " + jsonData.Year,
+              "IMDB rating: " + jsonData.imdbRating,
+              "Rotten Tomatoes rating: " + jsonData.Ratings[1].Value,
+              "Country Where Movie was Produced: " + jsonData.Country,
+              "Language: " + jsonData.Language,
+              "Plot: " + jsonData.Plot,
+              "Actors: " + jsonData.Actors
+            ].join("\n\n");
+      
+            fs.appendFile("log.txt", movieData + divider, function(err) {
+              if (err) throw err;
+              console.log("\n",movieData);
+            });
+          })};
+        };
+        omdb();
+        break;
+      
+
+  
+  }});
+}
+
+else {
+
+switch (primaryArg) {
 // * `concert-this`
+  case "concert-this":
+  
+  function bandsInTown() {
 
+    bandsURL = "https://rest.bandsintown.com/artists/" + secondArg + "/events?app_id=codingbootcamp";
+
+    request(bandsURL, function(err, response, body) {
+        let jsonData = JSON.parse(body);
+
+        let artistData = [
+          "Venue: " + jsonData[0].venue.name,
+          "Location: " + jsonData[0].venue.city + ", " + jsonData[0].venue.region,
+          "Date: " + moment(jsonData[0].datetime).format("MM/DD/YYYY")
+
+        ].join("\n\n");
+
+        fs.appendFile("log.txt", artistData + divider, function(err) {
+          if (err) throw err;
+          console.log("\n", artistData);
+        });
+    });
+
+  };
+
+  bandsInTown();
+  break;
 // * `spotify-this-song`
- 
-spotify.search({ type: 'track', query: 'Expectations' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
-// * `movie-this`
+  case "spotify-this-song":
 
-// * `do-what-it-says`
+  
+  if (secondArg === "") {
+    secondArg = "The Sign"
+
+    spotify.search({ type: "track", query: secondArg }, function(err, data) {
+
+      let spotifyData = [
+        "Artist: " + data.tracks.items[7].album.artists[0].name,
+        "Name: " + data.tracks.items[7].name,
+        "Preview: " + data.tracks.items[7].preview_url,
+        "Album: " + data.tracks.items[7].album.name,
+      ].join("\n\n");
+  
+      fs.appendFile("log.txt", spotifyData + divider, function(err) {
+        if (err) throw err;
+        console.log("\n",spotifyData);
+      });
+    });
+  }
+  
+  else {
+    spotify.search({ type: "track", query: secondArg }, function(err, data) {
+
+      let spotifyData = [
+        "Artist: " + data.tracks.items[0].album.artists[0].name,
+        "Name: " + data.tracks.items[0].name,
+        "Preview: " + data.tracks.items[0].preview_url,
+        "Album: " + data.tracks.items[0].album.name,
+      ].join("\n\n");
+
+      fs.appendFile("log.txt", spotifyData + divider, function(err) {
+        if (err) throw err;
+        console.log("\n",spotifyData);
+      });
+    })};
+  break;
+
+// * `movie-this`
+  case "movie-this":
+
+  function omdb() {
+
+
+    let movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + secondArg;
+
+    if (secondArg === "") {
+      
+      let movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=Mr.Nobody"
+
+    request(movieURL, function(err, response, body) {
+      let jsonData = JSON.parse(body);
+
+      let movieData = [
+        "Title: " + jsonData.Title,
+        "Release Year: " + jsonData.Year,
+        "IMDB rating: " + jsonData.imdbRating,
+        "Rotten Tomatoes rating: " + jsonData.Ratings[1].Value,
+        "Country Where Movie was Produced: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Actors: " + jsonData.Actors
+      ].join("\n\n");
+
+      fs.appendFile("log.txt", movieData + divider, function(err) {
+        if (err) throw err;
+        console.log("\n",movieData);
+      });
+    })}
+
+    else {
+
+    request(movieURL, function(err, response, body) {
+      let jsonData = JSON.parse(body);
+
+      let movieData = [
+        "Title: " + jsonData.Title,
+        "Release Year: " + jsonData.Year,
+        "IMDB rating: " + jsonData.imdbRating,
+        "Rotten Tomatoes rating: " + jsonData.Ratings[1].Value,
+        "Country Where Movie was Produced: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Actors: " + jsonData.Actors
+      ].join("\n\n");
+
+      fs.appendFile("log.txt", movieData + divider, function(err) {
+        if (err) throw err;
+        console.log("\n",movieData);
+      });
+    })};
+  };
+  omdb();
+  break;
+}};
